31956aab23286b1b3ece3ea033d1964b02fa2463
yan.jia <crazystone13753251592@gmail.com>
2024-01-10 22:56:57 +0800

    commit

--- src/ui.ts
+++ src/ui.ts
@@ -1,18 +1,18 @@
-// import { log, search } from 'vistopia-api';
+import { log, search } from 'vistopia-api';
 
 function createSearchContainer(): HTMLElement {
     const searchContainer = document.createElement('div');
-    searchContainer.innerHTML = "vistopia extension";
+    // searchContainer.innerHTML = "vistopia extension";
 
     // text input
     const textInput = document.createElement('input');
     // textInput.addEventListener('')
 
     const searchButton = document.createElement('button');
-    searchButton.innerHTML = "搜索";
+    searchButton.innerHTML = "search";
     searchButton.addEventListener('click', async (e) => {
         const searchWord = textInput.value;
-        // const res = await search(searchWord);
+        const res = await search(searchWord);
         // log(res);
     });
 
